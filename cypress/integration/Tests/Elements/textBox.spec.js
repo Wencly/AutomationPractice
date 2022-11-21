d680095047f39f730c862af5632e31e67e9bd9d6
Wencly <wendychuquimia2020@gmail.com>
2022-11-20 21:19:51 -0400

    Add options in Elements

--- cypress/integration/Tests/Elements/textBox.spec.js
+++ cypress/integration/Tests/Elements/textBox.spec.js
@@ -1,6 +1,16 @@
 describe('Name of the group', () => {
     it('should ', () => {
         cy.visit('/text-box');
+        //Full Name
+        cy.wait(5000)
         cy.get('input[id="userName"]').should('be.visible').type('My name is');
+        //Email
+        /* cy.get('#userEmail').type('wendychuquimia@gmail.com')
+        //Current Address
+        cy.get('#currentAddress').type('Calle Siempre viva Nro 123')
+        //Permanent Address
+        cy.get('#permanentAddress').type('Calle Siempre viva Nro 123')
+        //submit
+        cy.get('#submit') */
     });
 });
